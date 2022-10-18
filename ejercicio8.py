96955278b50e4ccb3ba2b44546f2fddc7bd8d1ec
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2022-10-18 08:53:29 +0200

    Create ejercicio8.py

---
+++ ejercicio8.py
@@ -0,0 +1,9 @@
+# Ejercicio 8 #
+'''
+Denifinir una estructura de datos con listas que permita guardar la temperatura mínima y máxima de 5 días. 
+Realiza un programa que de la siguiente información:
+     - La temperatura media de cada día
+     - Los días con menos temperatura
+     - Que permita leer una temperatura por teclado y muestre los días cuya temperatura máxima coincida con ella, en caso contrario mostrar un mensaje diciendo que no existe ningún día
+     
+'''
