2d00e3bf6d7f5fe6837d2ea0ac10f50764177b0b
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2022-10-18 10:00:40 +0200

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -3,5 +3,31 @@
 Realizar un programa que inicialice una lista con 10 valores aleatorios (del 1 al 10) y
 posteriormente muestre en pantalla cada elemento de la lista junto con su cuadrado y su cubo.
 '''
-def programa():
-  pass
+# Ejercicio 1 #
+'''
+Realizar un programa que inicialice una lista con 10 valores aleatorios
+(del 1 al 10) y posteriormente muestre en pantalla cada
+elemento de la lista junto con su cuadrado y su cubo.
+'''
+import random
+
+# Es una función que devuelve una lista con 10 números aleatorios entre 0 y 10
+def generarListaAleatoria():
+    lst = []
+    for i in range(0, 10):
+        lst.append(random.randint(0,10))
+    return lst
+
+# Es una fución que calcula el cuadrado de un número
+def cuadrado(x):
+    return x * x
+
+# Es una fucnión que calcula el cubo de un número
+def cubo(x):
+    return x * x * x
+
+def ejercicio1():
+    lista = generarListaAleatoria()
+    
+    for i in lista:
+        print(i , " cuadrado -> ", cuadrado(i), " cubo --> ", cubo(i))
