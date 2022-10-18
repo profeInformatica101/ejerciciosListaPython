3de69b291a65d8902bde023e6c39ac1582f3c4fe
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2022-10-18 08:43:45 +0200

    Create ejercicio7.py

---
+++ ejercicio7.py
@@ -0,0 +1,11 @@
+# Ejercicio 7 #
+'''
+Programa que declare tres listas 'lista1', 'lista2' de cinco enteros cada uno. 
+Calcule lista3 = lista1 + lista2, por ejemplo 
+
+           lista1 = [1, 2, 3, 4, 5]
+        +  lista2 = [3, 2, 4, 5, 1]
+           --------------------------
+           lista3 = [4, 4, 7, 9, 6]
+           
+'''
