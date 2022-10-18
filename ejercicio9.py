cfc42ffc0986a087ce439b7d879fcc608cd5a4ab
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2022-10-18 09:04:30 +0200

    Create ejercicio9.py

---
+++ ejercicio9.py
@@ -0,0 +1,23 @@
+# Ejercicio 9 #
+
+'''
+Implementar la siguientes funciones 
+        - sumaVector(<Lista>) devuelve <Entero>.
+        - sumaVectorDeVector(<Lista>) devuelve <Entero>.
+        
+      
+              
+'''
+
+# Dado una lista con números enteros devolver la suma de todos ellos por ejemplo:
+              # sumaVector([1,2,3]) -->  6
+              # sumaVector([3,4]) --> 7
+def sumaVector(lst):
+  pass
+
+
+# Dado una lista con listas de números enteros devolver la suma de todos ellos por ejemplo:
+              # sumaVectorDeVector([[1,2],[3,4],[5,6,7]]) --> 28
+
+def sumaVectorDeVector(lst):
+  pass
