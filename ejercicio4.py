eac363f08607658a34f1fff3ffa412d51546fae1
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2022-10-18 08:34:35 +0200

    Create ejercicio4.py

---
+++ ejercicio4.py
@@ -0,0 +1,5 @@
+# Ejercicio 4 #
+'''
+Programa que declare una lista y la vaya llenando de núemros hasta que introducimos un número negativo.
+Entonces se debe imprmir el vector (sólo los elementos introducidos)
+'''
