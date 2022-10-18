a41afbaf87df50e0bb83f93b6dc8e85fafe64c90
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2022-10-18 08:31:38 +0200

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,6 @@
+# Ejercicio 2
+'''
+Crea una lista e inicializala con 5 cadenas de caracteres leídas por teclado. 
+Copia los elementos de la lista en otra lista pero en orden inverso, y muestra sus elementos por la pantalla.
+'''
+
