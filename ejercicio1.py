881e0ee3975d0c70c1b1e183611972e9f671397e
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2022-10-18 09:31:52 +0200

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -3,3 +3,5 @@
 Realizar un programa que inicialice una lista con 10 valores aleatorios (del 1 al 10) y
 posteriormente muestre en pantalla cada elemento de la lista junto con su cuadrado y su cubo.
 '''
+def programa():
+  pass
