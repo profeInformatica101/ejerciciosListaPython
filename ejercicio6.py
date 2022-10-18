fc80e29cf87548585b882a787e5961f2bdb038b4
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2022-10-18 08:38:53 +0200

    Create ejercicio6.py

---
+++ ejercicio6.py
@@ -0,0 +1,6 @@
+# Ejercicio 6 #
+'''
+Crea un programa que pida un número al usuario, un número de mes ( por ejemplo, el 4). 
+El programa dirá cuántos días tiene (por ejemplo 30) y el nombre del mes. Debes usar listas. Nota: En una lista podemos guardar listas.
+Para simplificarlo vamos a suponer que febrero tiene 28 días
+'''
