2dc64a5eb22d0b056c3bac680f1500e1cb8092d1
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2022-10-21 11:19:01 +0200

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -3,17 +3,6 @@
 Se quiere realizar un programa que lea por teclado las 5 notas obtenidas por un alumno (comprendidas entre 0 y 10).
 A continuación debe mostrar todas las notas, la nota media, la nota más alta que ha sacado y la menor
 '''
-# Ejercicio 3 #
-'''
-Se quiere realizar un programa
-que lea por teclado las 5 notas
-obtenidas por un alumno
-(comprendidas entre 0 y 10).
-A continuación debe mostrar
-todas las notas, la nota media,
-la nota más alta que ha sacado y la menor
-'''
-
 def ejercicio3():
     #Un listado con 5 notas
     listado = obtenerNotas(5)
